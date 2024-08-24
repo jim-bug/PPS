73da642fa9472718a91362e0478a8739dd8ce1be
jim_bug <ignazioandsperandeo@gmail.com>
2024-08-24 23:33:06 +0200

    Modifica del codice, tolto il for dal void loop(), codice funzionante!

--- PPS_code/PPS_code.ino
+++ PPS_code/PPS_code.ino
@@ -1,15 +1,15 @@
 /*
- * Autore: Ignazio Leonardo Calogero Sperandeo
- * Data: 27/12/2023
- * Progetto: Particular Power Strip(PPS)
+ * Autor: Ignazio Leonardo Calogero Sperandeo
+ * Date: 27/12/2023
+ * Project: Particular Power Strip(PPS)
  * by jim_bug :)
  * 
- * Comandi da CLI dal device:
- * 0: accende/spegne la presa 1
- * 1: accende/spegne la presa 2
- * 2: accende/spegne la presa 3
- * 3: accende/spegne la presa 4
- * N: mappa stato delle prese
+ * Command from device:
+ * 0: on/off socket 1
+ * 1: on/off socket 2
+ * 2: on/off socket 3
+ * 3: on/off socket 4
+ * N: States of socket
 */
 
 #include <SoftwareSerial.h>
@@ -18,7 +18,10 @@
 #define END_PIN 9
 SoftwareSerial btserial(0, 1);  // rx tx pin
 int pinPrese[LEN];
-int statusPrese[LEN];  // logica inversa, 0=true 1=false
+int statusPrese[LEN];   // reverse logic 0=true 1=false
+int printState = 0;
+int i = 0;
+char code;
 
 void setup(){
   Serial.begin(9600);
@@ -26,38 +29,44 @@ void setup(){
   // Serial.println("Particolar Power Strip by jim_bug :)");
   for(int i = START_PIN;i >= END_PIN; i--){
     pinPrese[START_PIN-i] = i;
-    statusPrese[START_PIN-i] = 1; // salvo i stati di tutte le prese a 1 ossia spente
+    statusPrese[START_PIN-i] = 1;   
     pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH); // diseccitto tutti i relè, quindi tutte le prese sono a stato 1
+    digitalWrite(i, HIGH);          // de-exciting every relay, so every socket state are HIGH
   }
 }
 
 void loop(){
-  if(btserial.available()){ // se arduino riceve qualcosa
-    int presa = btserial.read()-48;  // ricevo il dato grezzo, sottraendolo a 48(codice ascii di 0), ottengo l'indice inviato dal device
-    Serial.println(presa);
-    if(presa >= 0 && presa <= (LEN-1)){
-      if(statusPrese[presa] == 1){  // se la presa è spenta
-        digitalWrite(pinPrese[presa], LOW);   // eccito il relè
-        statusPrese[presa] = 0; // mi salvo lo stato della presa
-      }
-      else{
-        digitalWrite(pinPrese[presa], HIGH);
-        statusPrese[presa] = 1;
+  if(btserial.available()){       // blocking function
+    if(printState == 0){
+      code = btserial.read();
+      if(code == 'N'){
+        
+        printState = 1;  
       }
     }
-    else if(presa == 30){
-      for(int i = 0;i < LEN; i++){
+    // Serial.print("I've recived: ");
+    // Serial.println(code);
+    if(((int)code >= 48 && (int)code <= (48+LEN)-1)){     // modify the state of a choising socket
+        statusPrese[(int)code-48] = !statusPrese[(int)code-48];
+        digitalWrite(pinPrese[(int)code-48], statusPrese[(int)code-48]);
+    }
+  }
+  else if(printState){
+        if(i < LEN){
           if(statusPrese[i] == 1){
               Serial.print(i+1);
-              Serial.println(") Stato Presa: DISATTIVO");
+              Serial.println(") Socket State: DISACTIVE");
           }
           else{
               Serial.print(i+1);
-              Serial.println(") Stato Presa: ATTIVO");
+              Serial.println(") Socket State: ACTIVE");
           }
+          i++;
+      }
+      else{
+        i = 0;
+        printState = 0;
       }
     }
-  }
 }
 // :)
