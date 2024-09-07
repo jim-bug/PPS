ca7a3b9abda3823ba2eb31786ba86ef9c29f5221
jim-bug <119898897+jim-bug@users.noreply.github.com>
2024-09-07 23:28:40 +0200

    Update convertHtmlToEsp32.py

--- PPS_code/convertHtmlToEsp32.py
+++ PPS_code/convertHtmlToEsp32.py
@@ -12,5 +12,5 @@ from sys import argv
 with open(argv[0], 'r') as file:		# use python3 convertHtmlToEsp32 <name>.txt
 	for i in file:
 		print(f"client.println(\"{i.lstrip().rstrip().replace("\"", "\\\"")}\");")
-// :)
+# :)
 
