7ac35b76169c1f0dfe49c4d5e4e8426cd42d4a04
jim-bug <119898897+jim-bug@users.noreply.github.com>
2024-09-07 23:28:24 +0200

    Update convertHtmlToEsp32.py

--- PPS_code/convertHtmlToEsp32.py
+++ PPS_code/convertHtmlToEsp32.py
@@ -1,11 +1,11 @@
-/*
+"""
  Author: Ignazio Leonardo Calogero Sperandeo
  Date: 25/08/2024
  Project Name: PPSv2
  Repo: https://github.com/jim-bug/PPS/tree/pps-v2
 
  by jim_bug // :)
-*/
+"""
 
 from sys import argv
 
